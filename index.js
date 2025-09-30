628657590bc692ac46977043b83fa44f78641b83
Ryan Luttrull <reluttrull@gmail.com>
2025-09-30 16:29:38 -0500

    started work on filling out chords: can get even chord spacing for n voices

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const chords = require('./chords.json')
+const notes = require('./notes.json')
+
+function getThird(rootValue, chordType) {
+    switch (chordType) {
+        case "M":
+            return rootValue + 4;
+        case "m":
+            return rootValue + 3;
+    }
+}
+
+function getFifth(rootValue, chordType) {
+    switch (chordType) {
+        case "M":
+            return rootValue + 7;
+        case "m":
+            return rootValue + 7;
+    }
+}
 
 function getAllChordNotes(chordName) {
-    console.log(chords);
     // base notes are the lowest possible notes we can use to spell a chord
     // e.g. C0, E0, G0
-    let allChords = chords.find((chord) => chord.chordName == chordName).baseNoteValues;
+    let rootName = chords.find((chord) => chord.chordName == chordName).root;
+    let baseRoot = notes.find((note) => note.name == rootName).baseValue;
+    let chordType = chordName.slice(-1);
+    let allChords = [baseRoot % 12, getThird(baseRoot, chordType) % 12, getFifth(baseRoot, chordType) % 12];
     // fill out 9 total octaves of notes, starting at C0
     for (let i = 1; i <= 8; i++) {
         // for now, all chords have three notes
@@ -15,6 +36,35 @@ function getAllChordNotes(chordName) {
     return allChords;
 }
 
+// get starting evenly spaced relative positions
+function getVoiceStarts(voices) {
+    const spacing = 8;
+    let index = (voices - 1) * -4;
+    var voicels = [];
+    while (voices > 0) {
+        voicels.push(index);
+        index += spacing;
+        voices--;
+    }
+    return voicels;
+}
+
+// get {voices} nearest evenly-spaced notes for {chordName} chord
+function getChordVoices(chordName, voices) {
+    console.log("chordName = " + chordName + ", voice = " + voices);
+    var allChordNotes = getAllChordNotes(chordName);
+    // map center around middle C
+    var voicels = getVoiceStarts(voices).map((note) => note + 48);
+    for (let i = 0; i < voicels.length; i++) {
+        let pick = allChordNotes.reduce((nearest, num) => { return Math.abs(num - voicels[i]) < Math.abs(nearest - voicels[i]) ? num : nearest;});
+        voicels[i] = pick;
+        allChordNotes[allChordNotes.indexOf(pick)] = -1;
+    }
+    return voicels;
+}
+
 module.exports = {
-    getAllChordNotes
+    getAllChordNotes,
+    getVoiceStarts,
+    getChordVoices
 };
--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,4 @@
-const { getAllChordNotes } = require('../index.js');
+const { getAllChordNotes, getVoiceStarts, getChordVoices } = require('../index.js');
 
 test('C Major: all notes', () => {
     expect(getAllChordNotes("CM").toString()).toBe([0,4,7, 12,16,19, 24,28,31, 36,40,43, 48,52,55, 60,64,67, 72,76,79, 84,88,91, 96,100,103].toString());
@@ -6,4 +6,25 @@ test('C Major: all notes', () => {
 
 test('C minor: all notes', () => {
     expect(getAllChordNotes("Cm").toString()).toBe([0,3,7, 12,15,19, 24,27,31, 36,39,43, 48,51,55, 60,63,67, 72,75,79, 84,87,91, 96,99,103].toString());
+});
+
+test('get avg voice start positions for 3', () => {
+    expect(getVoiceStarts(3).toString()).toBe([-8, 0, 8].toString());
+});
+
+
+test('get avg voice start positions for 4', () => {
+    expect(getVoiceStarts(4).toString()).toBe([-12, -4, 4, 12].toString());
+});
+
+test('get start positions for BM, 1 voices', () => {
+    expect(getChordVoices("BM", 1).toString()).toBe([47].toString());
+});
+
+test('get start positions for Fm, 3 voices', () => {
+    expect(getChordVoices("Fm", 3).toString()).toBe([41, 48, 56].toString());
+});
+
+test('get start positions for CM, 4 voices', () => {
+    expect(getChordVoices("CM", 4).toString()).toBe([36, 43, 52, 60].toString());
 });
